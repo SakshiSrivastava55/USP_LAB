461f372010fc3631edeca3f429a3c0719ec1ff92
SakshiSrivastava55 <53784511+SakshiSrivastava55@users.noreply.github.com>
2020-11-12 14:18:38 +0530

    Update code.sh

--- PASS_STUDENT/code.sh
+++ PASS_STUDENT/code.sh
@@ -1,24 +1,59 @@
-echo "\n--------------------STUDENT MARKS--------------------\n"
-
-read -p "Enter Student Name : " name
-echo "Enter 3 subject marks of $name :"
-read m1 m2 m3
-sum1=`expr $m1 + $m2 + $m3`
-echo "\nTotal Score out of 300 : " $sum1
-per=`expr $sum1 / 3`
-echo "Total Percentage: $per% "
-if [ $per -ge 90 ]
-then
-echo "S Grade"
-elif [ $per -ge 75 ]
-then
-echo "A Grade"
-elif [ $per -ge  60 ]
-then
-echo "B Grade"
-elif [ $per -le  40 ]
-then
-echo "Fail"
-else
-echo "C Grade"
-fi
+clear 
+echo ----------------------------------- 
+echo '\tStudent Mark List' 
+echo ----------------------------------- 
+echo Enter the Student name 
+read name 
+echo Enter the USN number 
+read rno 
+echo Enter the Mark1 
+read m1 
+echo Enter the Mark2 
+read m2 
+echo Enter the Mark3 
+read m3 
+echo Enter the Mark4 
+read m4 
+echo Enter the Mark5 
+read m5 
+tot=$(expr $m1 + $m2 + $m3 + $m4 + $m5) 
+avg=$(expr $tot / 5) 
+echo ----------------------------------- 
+echo '\tStudent Mark List' 
+echo ----------------------------------- 
+echo "Student Name    : $name" 
+echo "Register Number : $rno" 
+echo "Mark1           : $m1" 
+echo "Mark2           : $m2" 
+echo "Mark3           : $m3" 
+echo "Mark4           : $m4" 
+echo "Mark5           : $m5" 
+echo "Total           : $tot" 
+echo "Average         : $avg" 
+if [ $m1 -ge 35 ] && [ $m2 -ge 35 ] && [ $m3 -ge 35 ] && [ $m4 -ge 35 ] && [ $m5 -ge 35 ] 
+then 
+ echo "Result          : Pass" 
+  
+ if [ $avg -ge 90 ] 
+ then 
+  echo "Grade           : S" 
+ elif [ $avg -ge 80 ] 
+ then 
+         echo "Grade           : A" 
+ elif [ $avg -ge 70 ] 
+ then 
+         echo "Grade           : B" 
+ elif [ $avg -ge 60 ] 
+ then 
+         echo "Grade           : C" 
+ elif [ $avg -ge 50 ] 
+ then 
+         echo "Grade           : D" 
+ elif [ $avg -ge 35 ] 
+ then 
+         echo "Grade           : E" 
+ fi 
+else 
+ echo "Result          : Fail" 
+fi 
+echo ----------------------------------- 
